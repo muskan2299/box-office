7e31977908454e1c44181b343d7ba46e2735b762
muskan <agarwal.muskan22@gmail.com>
2021-11-10 02:11:44 +0530

    Updates

---
+++ precache-manifest.9bae1a096cb905e81244b6e67a642997.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ab1daa13e42293ff03ee1187ef3b86",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "4915a7038f0e8d8b0088",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "8618f5928fa487b7c345",
+    "url": "/box-office/static/js/2.114e7238.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/box-office/static/js/2.114e7238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f41e34aab499937f03e",
+    "url": "/box-office/static/js/3.063e9dab.chunk.js"
+  },
+  {
+    "revision": "4915a7038f0e8d8b0088",
+    "url": "/box-office/static/js/main.106fb03d.chunk.js"
+  },
+  {
+    "revision": "243223da1a379f74e618",
+    "url": "/box-office/static/js/runtime-main.6e812b73.js"
+  }
+]);
